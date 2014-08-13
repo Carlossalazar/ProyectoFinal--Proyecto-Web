dcbb1ece472f6b888e52d20f477939cd9f4a9af2
Carlossalazar <carlossalazar1307@gmail.com>
2014-08-12 21:39:26 -0600

    Avances en Clase Carrera
    Cambio de método agregar en clase carrera

--- Proyecto/js/carreras.js
+++ Proyecto/js/carreras.js
@@ -1,7 +1,3 @@
-//ejemplo de agenda telefonica con localStoage
-//autor: cali rojas
-//blog: www.lewebmonster.com
-
 $(function(){
 	//verificamos si el navegador soporta localStorage
 	if(!localStorage){
@@ -11,32 +7,32 @@ $(function(){
 		});
 	}
 	
-	$.mostrarListaDeContactos=function(){
-		//guardamos en una variable la cantidad de contactos y el cuerpo de la
+	$.mostrarListaCarreras=function(){
+		//guardamos en una variable la cantidad de carreras y el cuerpo de la
 		//tabla en la que mostraremos la lista (agregando filas con jQuery)
-		var iTotalContactos=localStorage.length,
-		$objCuerpoTablaContactos=$('#tblTablaContactos').find('tbody');
+		var iTotalCarreras=localStorage.length,
+		$objcuerpoTablaCarrera=$('#tblTablacarreras').find('tbody');
 		
 		//vaciamos el cuerpo de la tabla
-		$objCuerpoTablaContactos.empty();
+		$objcuerpoTablaCarrera.empty();
 		
-		//hay contactos almacenados?
-		if(iTotalContactos>0){
-			//recorremos la lista de contactos (los items almacenados en localStorage)
-			for(var iContacto=0; iContacto<iTotalContactos; iContacto++){
-				//guardamos en variables el telefono y nombre recuperados del localStorage
-				var strTelefono=localStorage.key(iContacto),
+		//Verificamos si hay carreras almacenados?
+		if(iTotalCarreras>0){
+			//recorremos la lista de carreras (los items almacenados en localStorage)
+			for(var iContacto=0; iContacto<iTotalCarreras; iContacto++){
+				//guardamos en variables los datos recuperados del localStorage
+				var strCodigo=localStorage.key(iContacto),
 				strNombre=localStorage.getItem(localStorage.key(iContacto));
-				
-				//agregamos una nueva fila con los datos del contacto
-				$objCuerpoTablaContactos.append(
+			
+				//agregamos una nueva fila con los datos de la carrera
+				$objcuerpoTablaCarrera.append(
 					$('<tr>').append(
-						$('<td>',{ //fila con el nombre del contacto
+						$('<td>',{ //fila con el nombre de la carrera
 							text	: strNombre,
 							align	: 'left'
 						}),
-						$('<td>',{ //fila con el numero de telefono
-							text	: strTelefono,
+						$('<td>',{ //fila con el Codigo de Carrera de telefono
+							text	: strCodigo,
 							align	: 'left'
 						}),
 						$('<td>',{ //fila para el boton de eliminar
@@ -48,17 +44,17 @@ $(function(){
 								type	: 'button',
 								class	: 'clsEliminarContacto',
 								value	: 'Eliminar',
-							}).data('contactoAEliminar',strTelefono) //por medio del metodo
-							//data almacenamos en el boton el numero que debemos eliminar
+							}).data('contactoAEliminar',strCodigo) //por medio del metodo
+							//data almacenamos en el boton el Codigo de Carrera que debemos eliminar
 							//(esto no sera visible, es un truquillo interesante)
 						)
 					)
 				);
 			}
-		//no hay contactos almacenados
+		//no hay carreras almacenados
 		}else{
-			//agregamos una fila con un mensaje indicando que no hay contactos
-			$objCuerpoTablaContactos.append(
+			//agregamos una fila con un mensaje indicando que no hay carreras
+			$objcuerpoTablaCarrera.append(
 				$('<tr>').append(
 					$('<td>',{
 						text	: 'No se encuentran carreras',
@@ -73,43 +69,43 @@ $(function(){
 	//funcion para limpiar los campos del formulario
 	$.limpiarCamposDelFormulario=function(){
 		//vaciamos el contenido de los campos de texto
-		$('#txtNombre,#txtTelefono').val('');
+		$('#txtNombre,#txtCodigo').val('');
 		//enfocamos el campo para digitar el nombre
 		$('#txtNombre').focus();
 	};
 	
 	//evento submit del formulario
-	$('#frmAgregarContacto').on('submit',function(eEvento){
+	$('#frmAgregarCarrera').on('submit',function(eEvento){
 		//evitamos que el form se envie (para que no recargue la pagina)
 		eEvento.preventDefault();
 		
 		//obtenemos una "copia" de los campos de texto
-		var $txtTelefono=$('#txtTelefono'),$txtNombre=$('#txtNombre');
+		var $txtCodigo=$('#txtCodigo'),$txtNombre=$('#txtNombre');
 		
 		//verificamos que los datos no esten vacios
 		//con $.trim() eliminamos los espacios al final y al inicio de las cadenas
-		if($.trim($txtNombre.val())!='' && $.trim($txtTelefono.val())){
-			//creamos dos variables con el nombre y telefono que vamos a guardar
+		if($.trim($txtNombre.val())!='' && $.trim($txtCodigo.val())){
+			//creamos dos variables con el nombre y Codigo que vamos a guardar
 			var strNombre=$.trim($txtNombre.val()),
-			strTelefono=$.trim($txtTelefono.val());
+			strCodigo=$.trim($txtCodigo.val());
 			
-			//preguntamos si el numero de telefono ya existe
-			if(localStorage.getItem(strTelefono)){
-				//el numero existe... desea actualizar?
-				if(confirm('El número de teléfono ya existe ¿Desea actualizarlo?')){
+			//preguntamos si el Codigo de la carrera ya existe
+			if(localStorage.getItem(strCodigo)){
+				//el Codigo existe... desea actualizar?
+				if(confirm('El Codigo ya existe ¿Desea actualizarlo?')){
 					//actualizamos
-					localStorage.setItem(strTelefono,strNombre);
-					//cargamos en el cuerpo de la tabla la lista de contactos
-					$.mostrarListaDeContactos();
+					localStorage.setItem(strCodigo,strNombre);
+					//cargamos en el cuerpo de la tabla la lista de carreras
+					$.mostrarListaCarreras();
 					//limpiamos el formulario
 					$.limpiarCamposDelFormulario();
 				}
-			//el numero no existe
+			//el Codigo no existe
 			}else{
 				//agregamos el contacto al localStorage
-				localStorage.setItem(strTelefono,strNombre);
-				//cargamos en el cuerpo de la tabla la lista de contactos
-				$.mostrarListaDeContactos();
+				localStorage.setItem(strCodigo,strNombre);
+				//cargamos en el cuerpo de la tabla la lista de carreras
+				$.mostrarListaCarreras();
 				//limpiamos el formulario
 				$.limpiarCamposDelFormulario();
 			}
@@ -117,20 +113,20 @@ $(function(){
 			//verificamos si el nombre esta vacio
 			if($.trim($txtNombre.val())==''){
 				//mostramos un mensaje
-				alert('Por favor, digite el nombre del contacto.');
+				alert('Por favor, digite el nombre de la carrera.');
 				//enfocamos el campo para el nombre
 				$txtNombre.val('').focus();
 			//verificamos si el telefono esta vacio
 			}else{
 				//mostramos un mensaje
-				alert('Por favor, digite el número del contacto.');
+				alert('Por favor, digite el Codigo de carreras.');
 				//enfocamos el campo para el telefono
-				$txtTelefono.val('').focus();
+				$txtCodigo.val('').focus();
 			}
 		}
 	});
 	
-	//clic en el boton para eliminar un contacto
+	//clic en el boton para eliminar un carreras
 	//se usa live en vez de on, porque el boton se creo en tiempo de ejecucion
 	$('.clsEliminarContacto').live('click',function(){
 		//obtenemos el contacto que se va a eliminar (recordar que esta almacenado en data)
@@ -140,12 +136,13 @@ $(function(){
 			//eliminamos el contacto usando la clave que esta asociada al nombre
 			//recordemos que el item se guardo usando como clave el telefono
 			localStorage.removeItem(strTelefonoAEliminar);
-			//cargamos en el cuerpo de la tabla la lista de contactos
-			$.mostrarListaDeContactos();
+			//cargamos en el cuerpo de la tabla la lista de carreras
+			$.mostrarListaCarreras();
 		}
 	});
 	
-	//cuando la pagina carga mostramos la lista de contactos
+	//cuando la pagina carga mostramos la lista de carreras
 	//ojo: esto se hace al inicio...
-	$.mostrarListaDeContactos();
+	$.mostrarListaCarreras();
 });
+
